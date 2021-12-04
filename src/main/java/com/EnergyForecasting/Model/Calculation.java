93208110017f6130120d826df3b1a7909f06d975
NoahS-Upton <noah.upton@gmail.com>
2021-12-04 15:19:03 +0000

    updated calculation logic

--- src/main/java/com/EnergyForecasting/Model/Calculation.java
+++ src/main/java/com/EnergyForecasting/Model/Calculation.java
@@ -5,13 +5,39 @@ public class Calculation {
     public Calculation() {
     }
 
-    public static double solarOutput(double capacity, double MW2){
-            double output = capacity*MW2*0.75/60;
+    public static double solarHourlyOutput(double capacity, double hours){
+            double output = capacity*hours*0.75;
             return output;
         }
 
+    public static double solarRadOutput(double capacity, double rads){
+        double output = capacity*rads*0.75;
+        return output;
+    }
+
+
         public static double windOutput(double capacity, double windspeed){
-            double output=capacity*windspeed;
+            double output;
+            if (windspeed>50 && windspeed<=90){
+                output=capacity;
+            }else if(windspeed>18 && windspeed<=24){
+                output=capacity* 0.1;
+            }else if(windspeed>24 && windspeed<=28){
+                output=capacity* 0.2;
+            }else if(windspeed>28 && windspeed<=32){
+                output=capacity* 0.4;
+            }else if(windspeed>32 && windspeed<=36){
+                output=capacity* 0.5;
+            }else if(windspeed>36 && windspeed<=40){
+                output=capacity* 0.7;
+            }else if(windspeed>40 && windspeed<=45){
+                output=capacity* 0.8;
+            }else if(windspeed>45 && windspeed<=50){
+                output=capacity* 0.9;
+            }else{
+                output = 0.0;
+            }
+
             return output;
         }
 }
