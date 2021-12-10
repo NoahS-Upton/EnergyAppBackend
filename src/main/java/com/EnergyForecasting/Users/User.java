89d0bc5fbdb1bac3f05fa326b782c8c4db6c29c4
NoahS-Upton <noah.upton@gmail.com>
2021-12-10 12:04:04 +0000

    user commenting

--- src/main/java/com/EnergyForecasting/Users/User.java
+++ src/main/java/com/EnergyForecasting/Users/User.java
@@ -4,16 +4,19 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.persistence.*;
 
+
+/*
+    Entity for creating and storing user information
+ */
 @Getter
 @Setter
 @EqualsAndHashCode
 @Entity
 @Table(name ="User")
 public class User {
-//        implements UserDetails {
+//user properties
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
@@ -24,6 +27,8 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
+
+    //constructors
     @Autowired
     public User(String username, String password, String email) {
         this.username = username;
@@ -34,43 +39,35 @@ public class User {
     public User() {
     }
 
+
+    //getters and setters
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-
     public UserRole getRole() {
         return role;
     }
-
     public void setRole(UserRole role) {
         this.role = role;
     }
--- src/main/java/com/EnergyForecasting/Users/UserRole.java
+++ src/main/java/com/EnergyForecasting/Users/UserRole.java
@@ -1,5 +1,6 @@
 package com.EnergyForecasting.Users;
 
+//specifies types of user, these would be used in future developments
 public enum UserRole {
     GENERAL,
     PAID,
